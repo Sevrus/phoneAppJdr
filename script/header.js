a4bfb8f5ea62e8a6c52aad55cd3271a3d3b30758
Sevrus <book-n-troll@nordnet.fr>
2023-11-07 18:34:28 +0100

    reorganizing folders and files

---
+++ script/header.js
@@ -0,0 +1,15 @@
+const screenHours = document.querySelector('.hours');
+
+const updateClock = () => {
+    const date = new Date();
+    const tokyoTime = date.toLocaleTimeString('fr-FR', {
+        timeZone: 'Asia/Tokyo',
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+
+    screenHours.textContent = `${tokyoTime}`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
--- script/principal.js
+++ script/principal.js
@@ -1,19 +1,3 @@
-const screenHours = document.querySelector('.hours');
-
-const updateClock = () => {
-    const date = new Date();
-    const tokyoTime = date.toLocaleTimeString('fr-FR', {
-        timeZone: 'Asia/Tokyo',
-        hour: '2-digit',
-        minute: '2-digit',
-    });
-
-    screenHours.textContent = `${tokyoTime}`;
-}
-
-updateClock();
-setInterval(updateClock, 1000);
-
 const weatherIcons = {
     'Thunderstorm': 'wi wi-thunderstorm',
     'Drizzle': 'wi wi-sleet',
